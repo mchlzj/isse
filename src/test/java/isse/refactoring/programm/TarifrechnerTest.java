62abb11c917dca57335585197fca9c3f4209e53e
mchlzj <mchlzj@gmail.com>
2022-10-02 14:07:13 +0200

    korrektur von Tests

--- src/test/java/isse/refactoring/programm/TarifrechnerTest.java
+++ src/test/java/isse/refactoring/programm/TarifrechnerTest.java
@@ -7,19 +7,34 @@ public class TarifrechnerTest {
 
 	@Test
 	public void sollteBusinessTarifBerechnen() {
+		//GIVEN
 		Tarifrechner tarifrechner = new Tarifrechner();
-		Assertions.assertEquals(tarifrechner.berechneBusinessTarif(100.00), 119.00);
+		Double leistungshoehe = 100.00;
+		//WHEN
+		Double businessTarif = tarifrechner.berechneBusinessTarif(leistungshoehe);
+		//THEN
+		Assertions.assertEquals(businessTarif, 119.00);
 	}
 	
 	@Test
 	public void solltePremiumTarifBerechnen() {
+		//GIVEN
 		Tarifrechner tarifrechner = new Tarifrechner();
-		Assertions.assertEquals(tarifrechner.berechnePremiumTarif(100.00),107.10);
+		Double leistungshoehe = 100.00;
+		//WHEN
+		Double premiumTarif = tarifrechner.berechnePremiumTarif(leistungshoehe);
+		//THEN
+		Assertions.assertEquals(premiumTarif,107.10);
 	}
 	
 	@Test
 	public void sollteFirmenkundenTarifBerechnen() {
+		//GIVEN
 		Tarifrechner tarifrechner = new Tarifrechner();
-		Assertions.assertEquals(tarifrechner.berechneFirmenkundenTarif(100.00), 95.20);
+		Double leistungshoehe = 100.00;
+		//WHEN
+		Double firmenkundenTarif = tarifrechner.berechneFirmenkundenTarif(leistungshoehe);
+		//THEN
+		Assertions.assertEquals(firmenkundenTarif, 95.20);
 	}
 }
