e4d668a4af15e49bb9127cfcc7607ebf1cf63be5
mchlzj <mchlzj@gmail.com>
2022-10-02 14:14:33 +0200

    korrektur Tests 2

--- src/test/java/isse/refactoring/programm/ProgrammTest.java
+++ src/test/java/isse/refactoring/programm/ProgrammTest.java
@@ -4,45 +4,39 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ProgrammTest {
-
 	@Test
 	public void sollteBeiAuswahl1BusinessTarifBerechnen() {
-		Programm programm = new Programm();
-		
 		//GIVEN
+		Programm programm = new Programm();		
 		Double leistungshoehe = 100.00;
-		
-		//WHEN
 		Integer businessTarif = 1;
-		
+		//WHEN
+		Double preisBusinessTarif = programm.berechneTarif(businessTarif, leistungshoehe);
 		//THEN
-		Assertions.assertEquals(programm.berechneTarif(businessTarif,leistungshoehe), 119.00);
+		Assertions.assertEquals(preisBusinessTarif, 119.00);
 	}
 	
 	@Test
 	public void sollteBeiAuswahl2PresmiumTarifBerechnen() {
-		Programm programm = new Programm();
-		
 		//GIVEN
+		Programm programm = new Programm();
 		Double leistungshoehe = 100.00;
-		
-		//WHEN
 		Integer premiumTarif = 2;
-		
+		//WHEN
+		Double preisPremiumTarif = programm.berechneTarif(premiumTarif, leistungshoehe);
 		//THEN
-		Assertions.assertEquals(programm.berechneTarif(premiumTarif, leistungshoehe), 107.10);
+		Assertions.assertEquals(preisPremiumTarif, 107.10);
 	}
 	
-	@Test void sollteBeiAuswahl3FirmenkundenTarifBerechnen() {
-		Programm programm = new Programm();
-		
+	@Test 
+	public void sollteBeiAuswahl3FirmenkundenTarifBerechnen() {
 		//GIVEN
+		Programm programm = new Programm();
 		Double leistungshoehe = 100.00;
-		
-		//WHEN
 		Integer firmenkundenTarif = 3;
-		
+		//WHEN
+		Double preisFirmenkundenTarif = programm.berechneTarif(firmenkundenTarif, leistungshoehe);
 		//THEN
-		Assertions.assertEquals(programm.berechneTarif(firmenkundenTarif, leistungshoehe), 95.20);
+		Assertions.assertEquals(preisFirmenkundenTarif, 95.20);		
 	}
 }
